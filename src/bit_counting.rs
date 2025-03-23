6ef427227d262b8dd26e7ea2a1623a4de273d348
Boris Skert <boris.skert@gmail.com>
2025-03-23 16:23:14 +0100

    Solve 'Bit Counting' kata

---
+++ src/bit_counting.rs
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/526571aae218b8ee490006f4/train/rust
+pub fn count_bits(n: i64) -> u32 {
+    n.count_ones()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -33,3 +33,4 @@ pub mod sum_of_digits_digital_root;
 pub mod high_and_low;
 pub mod descending_order;
 pub mod return_negative;
+pub mod bit_counting;
